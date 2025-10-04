dadf6cf92c2a729728d4f35f098eb2c41223573b
ajlevin <ase.levine@gmail.com>
2025-10-04 11:49:32 -0700

    connected front and back

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -67,6 +68,7 @@ const users = {
   ]
 };
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => {
